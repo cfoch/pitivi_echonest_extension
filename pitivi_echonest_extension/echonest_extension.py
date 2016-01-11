d5cfb42d264e02a9c56d3a18f0167e0569e4770a
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2016-01-11 06:10:34 +0100

    Add clip dialog ui
    And expose full clip analysis

--- pitivi_echonest_extension/echonest_extension.py
+++ pitivi_echonest_extension/echonest_extension.py
@@ -1,10 +1,20 @@
-import threading
+import os, threading
 
 from gi.repository import Gtk, GLib
 from pyechonest import track
 
 from pitivi.extensions import BaseExtension
 from pitivi.medialibrary import COL_URI
+
+here = os.path.dirname(__file__)
+
+METADATA_BLACKLIST = ("pyechostring", "codestring", "synchstring",
+        "analysis_url", "rhythmstring", "echoprintstring", "meta",
+        "_object_type", "audio_md5", "cache", "code_version",
+        "decoder_version", "echoprint_version", "id", "md5",
+        "rhythm_version", "sample_md5", "status", "synch_version")
+
+LIST_TYPED_METADATA = ("segments", "tatums", "beats", "bars", "sections")
 
 class EchonestExtension(BaseExtension):
     EXTENSION_NAME = 'echonest-extension'
@@ -14,19 +24,72 @@ class EchonestExtension(BaseExtension):
         self.__asset_menu_item = None
         self.__analysis_handler_id = 0
 
+        self.analysed_tracks = {}
+
     def setup(self):
         self.app.gui.medialibrary.connect('populating-asset-menu',
                 self.__add_asset_menu_item_cb)
+        self.app.gui.timeline_ui.timeline.connect('populating-clip-menu',
+                self.__add_clip_menu_item_cb)
 
     def analysis_worker(self, filename):
         pytrack = track.track_from_filename(filename)
         pytrack.get_analysis()
-        print ("Found da track", dir(pytrack))
+        self.analysed_tracks[filename] = pytrack
+        return pytrack
 
     def __analyse_track(self, filename):
         t = threading.Thread(target=self.analysis_worker, args=(filename,))
         t.daemon = True
         t.start()
+
+    def __add_clip_menu_item_cb(self, timeline, clip, menu):
+        menu_item = Gtk.MenuItem.new_with_label("Echonest dialog")
+        menu_item.connect('activate',
+                self.__clip_dialog_cb, clip)
+        menu.append(menu_item)
+
+    def __fill_metadata_list(self, builder, track):
+        listbox = builder.get_object('metadata-list')
+        for name, value in sorted(track.__dict__.items()):
+            if name in METADATA_BLACKLIST:
+                continue
+
+            if name in LIST_TYPED_METADATA:
+                text = "Number of %s : %d" % (name, len(value))
+            else:
+                text = "%s : %s" % (name, str(value))
+
+            label = Gtk.Label.new(text)
+            label.set_halign (Gtk.Align.START)
+
+            if name in LIST_TYPED_METADATA:
+                listbox.prepend(label)
+            else:
+                listbox.insert(label, -1)
+
+        listbox.show_all()
+
+    def __clip_dialog_cb(self, widget, clip):
+        clip = clip.bClip
+        filename = GLib.filename_from_uri(clip.props.uri)[0]
+        track = self.analysed_tracks.get(filename)
+
+        if not track:
+            print ("Analysing sync")
+            track = self.analysis_worker(filename)
+
+
+        builder = Gtk.Builder()
+        builder.add_from_file(os.path.join(here, 'clip-dialog.ui'))
+        builder.connect_signals(self)
+        dialog = builder.get_object('clip-dialog')
+        dialog.set_transient_for(self.app.gui)
+        self.__fill_metadata_list(builder, track)
+        res = dialog.run()
+
+        # We gud
+        dialog.destroy()
 
     def __add_asset_menu_item_cb(self, medialibrary, model_row, menu):
         menu_item = Gtk.MenuItem.new_with_label("Run echonest analysis")
@@ -35,7 +98,6 @@ class EchonestExtension(BaseExtension):
         menu.append(menu_item)
 
     def __run_analysis_clicked_cb(self, widget, asset_uri):
-        print ("I will now run analysis amigo", asset_uri)
         self.__analyse_track(GLib.filename_from_uri(asset_uri)[0])
 
 def get_extension_classes():
