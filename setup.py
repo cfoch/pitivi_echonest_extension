cfd6024faf1e36a1b257a1537fb03aa66ac88b14
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2016-01-12 19:32:56 +0100

    Include ui files in sdist

--- setup.py
+++ setup.py
@@ -12,7 +12,9 @@ setup(
     packages = find_packages(),
 
     entry_points = {'pitivi.extensions': 'get_extension_classes = pitivi_echonest_extension.echonest_extension:get_extension_classes'},
-
+    package_data = {
+        '': ['*.ui'],
+    },
     dependency_links = \
             ['http://github.com/LuminosoInsight/ordered-set/tarball/master#egg=ordered_set-2.0.0',
              'http://github.com/MathieuDuponchelle/pyechonest3/tarball/py3#egg=pyechonest-9.0.3'],
